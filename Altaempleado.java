c4923ddad153ee5ab786b1e6205fe525be7eb6cf
SagaraJC <72108763+SagaraJC@users.noreply.github.com>
2021-03-01 21:54:15 -0600

    Add files via upload

--- Altaempleado.java
+++ Altaempleado.java
@@ -7,7 +7,7 @@
 package mx.unam.aragon.fes.gui;
 
 import mx.unam.aragon.fes.Empleado;
-
+import mx.unam.aragon.fes.Direccion;
 /**
  *
  * @author ryzen 5
@@ -421,13 +421,22 @@ public class Altaempleado extends javax.swing.JFrame {
         System.out.println("Nuevo empleado!!!");
         this.limpiarFormulario();
         Empleado emp=new Empleado();
-        emp.setNombre(this.TextField1.getText());
+        emp.setNombre(this.jTextField1.getText());
         emp.setApPaterno(this.jTextField2.getText());
         emp.setApMaterno(jTextField3.getText());
         emp.setEdad( Integer.parseInt(jTextField4.getText() ) ); //Convierte a int
-        emp.SetCurp(jTextField5.getText());
-        emp.setDomicilio().setCalle(this.jTextField6.getText());
-        
+        emp.setCurp(jTextField5.getText());
+        emp.setDomicilio(new Direccion());
+        emp.getDomicilio().setCalle(this.jTextField6.getText());
+        emp.getDomicilio().setNumero(this.jTextField7.getText());
+        emp.getDomicilio().setColonia(this.jTextField8.getText());
+        emp.getDomicilio().setDelegacion(this.jTextField9.getText());
+        emp.getDomicilio().setEstado(this.jTextField10.getText());
+        emp.setNumeroEmpleado(Integer.parseInt(this.jTextField11.getText()));
+        emp.setDepartamento(this.jTextField12.getText());
+        emp.setSueldo(Float.parseFloat(this.jTextField13.getText()));
+        emp.setHorasExtra(Integer.parseInt(this.jTextField14.getText()));
+        int confirmar = JOptionPane.showConfirmDialog(this, "¿Deseas guardar estos datos?");
     }//GEN-LAST:event_jButton1MouseClicked
 
     private void limpiarFormulario(){
